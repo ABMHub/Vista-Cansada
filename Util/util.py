b7605856285e9844d7c8f0773dc611f86bb83d0e
Lucas ABM <lucasabmacedo@hotmail.com>
2021-10-20 09:21:58 -0300

    Filtra frames ruins

---
+++ Util/util.py
@@ -0,0 +1,2 @@
+def ratio (n1 : float, n2 : float) -> float:
+  return min(n1, n2)/max(n1,n2)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+from Util.util import ratio
 
 captura = cv2.VideoCapture(0)
 calibrado = False
+encerrar = False
 
-while (calibrado is False):
+while (calibrado is False and encerrar is False):
   ret, frame_orig = captura.read()
   cv2.imshow('webcam', frame_orig)
 
@@ -13,13 +15,13 @@ while (calibrado is False):
     base_image = frame_orig
     calibrado = True
   if key == 27:
-    break
+    encerrar = True
 
 base_nomr = np.linalg.norm(base_image)
 orb_base = cv2.ORB_create()
 b_keypoints, b_des = orb_base.detectAndCompute(base_image, None)
 
-while(1):
+while(encerrar is False):
   ret, frame_orig = captura.read()
   frame = frame_orig
 
@@ -36,19 +38,23 @@ while(1):
   matches = matches[:numGoodMatches]
   points1 = np.zeros((len(matches), 2), dtype=np.float32)
   points2 = np.zeros((len(matches), 2), dtype=np.float32)
+  
   for i, match in enumerate(matches):
     points1[i, :] = f_keypoints[match.queryIdx].pt
     points2[i, :] = b_keypoints[match.trainIdx].pt
+
   try:
-    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
-    # print('ugauga')
+    h, _ = cv2.findHomography(points1, points2, cv2.RANSAC)
     height, width, _ = base_image.shape
-    im1Reg = cv2.warpPerspective(frame, h, (width, height))
+    temp = cv2.warpPerspective(frame, h, (width, height))
+    if ratio(np.linalg.norm(temp), base_nomr) > 0.8: im1Reg = temp
+
   except:
     print('deu ruim\n')
-  print(np.linalg.norm(im1Reg), base_nomr) # tenta 95%
+
   cv2.imshow('teste2', im1Reg)
   cv2.imshow('teste', final_img)
+
   key = cv2.waitKey(30)
   if key == 27:
-    break
+    encerrar = True
